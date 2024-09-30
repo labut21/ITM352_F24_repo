162f131e11199cedc6462b33cdd2c3a4b9af52d5
labut21 <labut@hawaii.edu>
2024-09-29 18:42:00 -1000

    Delete Ex4.py

--- Ex4.py
+++
@@ -1,8 +0,0 @@
-# Ask user for a number using stdin
-number = float(input("Enter a decimal formatted number between 1 and 10: "))
-
-# Round the values to two decimal places
-square_num = round(number ** 2, 2)
-
-# Print out the square of the input
-print(f"The square of "+ str(number) + " is " + str(square_num) )
