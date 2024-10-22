47605e6c86ac5a3cfa7c3a99fedb320e8914fb75
labut21 <labut@hawaii.edu>
2024-10-21 20:54:01 -1000

    Commit

--- Lab11.Pivot/Ex1.py
+++ Lab11.Pivot/Ex1.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import pyarrow # not needed, but it's a good practice to import it
 
+# The file at this URL contains a large data set. It must be downloadable and in CSV format to be read by pandas.
 url = "https://drive.google.com/uc?id=1ujY0WCcePdotG2xdbLyeECFW9lCJ4t-K"
 
-df = pd.read_csv(url)
+# Set display option to show all columns
+pd.set_option('display.max_columns', None)
 
-print(df.head())
+try:
+    # Attempt to read the CSV file
+    print(f"Reading CSV file...")
+    sales_data = pd.read_csv(url, dtype_backend='pyarrow', on_bad_lines='skip')
+
+    # We ask Pandas to parse the order_date field to turn it into a standard representation.
+    sales_data['order_date'] = pd.to_datetime(sales_data['order_date'], format='mixed')
+
+    # Print the first 5 rows
+    print(sales_data.head())
+
+except Exception as e:
+    print(f"An error occurred: {e}")
