bd4c1b21509021f36be62a4d25279b4b198b51c8
labut21 <labut@hawaii.edu>
2024-11-19 14:24:20 -1000

    Commit

--- Lab 13.Visualization/Ex1a.py
+++ Lab 13.Visualization/Ex1a.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_json('https://drive.google.com/file/d/1-kvj2Ore88PGzZ9J7_lPBOvNf5C1ohpQ')
+df = pd.read_json('https://drive.google.com/uc?id=1-kvj2Ore88PGzZ9J7_lPBOvNf5C1ohpQ')
 
 x = df(['trip_miles'])
 plt.hist(x)
--- Lab 13.Visualization/Ex2b.py
+++ Lab 13.Visualization/Ex2b.py
@@ -1,7 +1,1 @@
-import json
-import matplotlib.pyplot as plt
-import pandas as pd
-
-unique_values = df['']
-
 
