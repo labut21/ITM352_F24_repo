28853d3a99cdb5f2728b0881e134cf08769bc59f
labut21 <labut@hawaii.edu>
2024-11-21 11:46:30 -1000

    Commit

--- Lab 13.Visualization/Ex2b.py
+++ Lab 13.Visualization/Ex2b.py
@@ -1,1 +1,21 @@
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
 
+pd.set_option('display.max_rows', None)
+df = pd.read_json('https://drive.google.com/uc?id=1-kvj2Ore88PGzZ9J7_lPBOvNf5C1ohpQ')
+
+# Convert tips to numeric
+df = df.replace("Nan", pd.Na). dropna()
+
+number_cols = ['fare', 'tips']
+df['tips'] = pd.to_numeric(df['tips'])
+
+# Group by payment type and sum tips
+tips_by_payment = df.groupby('payment_type')['tips'].sum()
+
+plt.bar(list(tips_by_payment.index), tips_by_payment.tolist())
+plt.title('Total tips by Payment Method', fontsize=14)
+plt.xlabel('Payment Method', fontsize=12)
+plt.ylabel('Total Tips', fontsize=12)
+plt.show()
---
+++ Lab 13.Visualization/Ex4a.py
@@ -0,0 +1,24 @@
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+
+data_URL = 'https://drive.google.com/file/d/1-kvj2Ore88PGzZ9J7_lPBOvNf5C1ohpQ'
+df = pd.read_json(data_URL)
+
+# Convert tips to numeric
+number_cols = ['fare', 'tips',]
+df[number_cols] = df[number_cols].astype(float)
+
+# Drop missing values from fare and tip
+df = df.replace("Nan", pd.Na). dropna()
+
+# Make the scatter plot of fare vs tips
+tips = df(['fare]'], df['tips'])
+plt.scatter(df['fare'], df['trip_miles'])
+plt.title('Tips vs Fares', fontsize=14)
+plt.xlabel('Fares', fontsize=12)
+plt.ylabel('Tips', fontsize=12)
+
+plt.show()
